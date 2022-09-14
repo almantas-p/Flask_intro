0eb7fbee719d3dd15218cf5532e40e51ccd09dc1
Almantas P <almantas.p7@gmail.com>
2022-09-14 16:03:52 +0300

    with variable

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '<h1>Cia mano pirmasis Flask puslapis</h1><br><h2>antra eilute</h2>'
+@app.route('/<vardas>')
+def home(vardas):
+    return f'Labas, {vardas}'
 
 if __name__ == '__main__':
     app.run(debug=True)
